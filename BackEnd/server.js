ac8fb8d96f92452e8d394f6c58debc3816b7802d
phatdatng7804 <nguyenpdat782004@gmail.com>
2025-05-18 23:42:36 +0700

    Fix server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -21,7 +21,7 @@ app.use("/category", require("./src/routes/category"));
 app.use("/restaurant", require("./src/routes/restaurant"));
 app.use("/admin", require("./src/routes/admin"));
 app.use("/cart", require("./src/routes/cart"));
-app.use("/reports", require("./src/routes/report"));
+app.use("/reports", require("./src/routes/reports"));
 
 const server = http.createServer(app);
 const io = require("socket.io")(server, {
