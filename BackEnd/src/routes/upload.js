ce9eee06aae5e5ccfee08130afef10e60868abac
phatdatng7804 <nguyenpdat782004@gmail.com>
2025-05-31 18:01:20 +0700

    Fix upload2

--- BackEnd/src/routes/upload.js
+++ BackEnd/src/routes/upload.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const { upload, uploadImage } = require("../controllers/uploadController");
 
 // Upload ảnh 1 file, field name là "file"
-router.post("/upload-image", upload.single("file"), uploadImage);
+router.post("/upload-image", upload.single("image"), uploadImage);
 
 module.exports = router;
